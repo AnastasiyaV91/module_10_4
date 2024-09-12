88fc30758600ceaa16673a8adb5e86378fd27dfe
AnastasiyaV91 <anastasiyakiseleva@bk.ru>
2024-09-13 00:15:50 +0300

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,48 @@
+from threading import Thread
+from queue import Queue
+from time import sleep       #  Модуль для задержки sleep
+from random import randint   #  Модуль случайных чисел randmint
+
+class Table:
+    def init(self, number):
+        self.number = number       # Номер стола
+        self.guest = None          #  Гость, который сидит за столом number
+
+class Guest(Thread):
+    def init(self, name=''):
+        super().init()         # Инициализация для потоков
+        self.name = name           # Имя посетителя ( str )
+
+    def run(self):                    #  Метод для задержки
+        sleep(randint(3, 10))   #  Задержка на случайное количество от 3 до 10 секунд
+
+class Cafe:
+    def init(self, *tables):
+        self.queue = Queue()             #  Очередь в виде списка
+        self.tables = list(tables)    #  Столы в кафе (в виде списка столов)
+
+    def guest_arrival(self, *guests):
+        for guest in guests:                      #  Для каждого гостя из списка гостей
+            for table in self.tables:             #  Для каждого стола из списка столов
+                if table.guest is None:           #  Если стол свободен (ищем свободный стол), то
+                    table.guest = guest           #  посетителю присваивается номер стола
+                    guest.start()                 #  Запускаем поток посетителя
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")  #  Выводим сообщение
+                    break                         #  Если посетителю определили стол, то выходим из цикла
+            else:                            #  Если свободный стол не найден, то
+                self.queue.put(guest)        # добавляем посетителя в очередь
+                print(f"{guest.name} в очереди")  #  Выводим сообщение
+
+    def discuss_guests(self):
+        while self.queue or any(table.guest is not None for table in self.tables):
+            #  Пока очередь self.queue не пуста или один из столов из списка занят
+            for table in self.tables:                   #  Для каждого стола из списка столов
+                if table.guest is not None and not table.guest.is_alive():  #  Если стол занят и поток table.guest завершен
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")   #  Выводим сообщение
+                    table.guest = None                            #  Указываем, что стол свободен
+                    print(f"Стол номер {table.number} свободен")            #  Выводим сообщение
+                if table.guest is None and self.queue:  #  Если стол свободен и очередь есть,то
+                    guest = self.queue.get()            #  забираем из списка первого посетителя с индексом [0]
+                    table.guest = guest                 #  Посетителю присваивается номер стола
+                    print(f"{guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+                    guest.start()                       #  Запускаем поток посетителя
